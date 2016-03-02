fad321502d94a72d49d0b311d7b6c4fdc625d66b
Haberling <1973266@linux05.mcs.sdsmt.edu>
2016-03-02 01:07:15 -0700

    Comments n things

--- ParseXML.java
+++ ParseXML.java
@@ -35,26 +35,47 @@ import org.jfree.ui.RefineryUtilities;
 public class ParseXML 
 {
 
+    /*
+    * Function: Main
+    * Author: Marcus Haberling
+    * Simple function for testing and debugging
+    */
     public static void main( String[] args )
     {
         List<WeatherPoint> list = parseDirectory("./Data");
     }
 
+    /*
+    * Function: parseDirectory
+    * Author: Marcus Haberling
+    * Goes through a supplied directory name reading in WeatherPoints
+    * from each xml file. The are all added to a list which is returned;
+    */
   	public static List<WeatherPoint> parseDirectory( String directory )
 	{
 		ArrayList<WeatherPoint> toReturn = new ArrayList<WeatherPoint>();
 		File folder = new File(directory);
 		File[] listOfFiles = folder.listFiles();
-		for(int i = 0; i < listOfFiles.length; i++)
+		for(int i = 0; i < listOfFiles.length; i++) //loop through files
 		{
 			String filename = listOfFiles[i].getAbsolutePath();
 			if(filename.endsWith(".xml")||filename.endsWith(".XML"))
 			{		
+                //if an xml file, call parseweather on it to get xml
 				toReturn.addAll(parseWeather(filename));
 			}
    		}
         return toReturn;
   	}
+
+    /*
+    * Function: parseWeather
+    * Author: Marcus Haberling
+    * Goes through a supplied filed and loads all the weatherpoints in
+    * the file and then puts them into a list wich is then returned
+    * this code was based off of the ParseXML2 example supplied by
+    * Dr. Weiss
+    */
     public static List<WeatherPoint> parseWeather( String file )
     {
         SAXBuilder builder;
@@ -65,12 +86,12 @@ public class ParseXML
     
         try
         {
-
+            //fill document and get list of 
             doc = builder.build( file );
             root = doc.getRootElement();
             List points = root.getChildren();
             Iterator pointsIterator = points.iterator();   
-        
+            //Loop through list calling addWeatherPoint on each element
             while( pointsIterator.hasNext() )
             {
 
@@ -96,6 +117,12 @@ public class ParseXML
         return toReturn;
     }
     
+    /*
+    * Function: addWeatherPoint
+    * Author: Marcus Haberling
+    * Goes through a supplied xml element and extracts all the data
+    * into a WeatherPoint object. Which is then returned.
+    */
     public static WeatherPoint addWeatherPoint( Element current )
     {
         List attributes = current.getChildren();
@@ -104,6 +131,7 @@ public class ParseXML
         String tempDate = "01/01/1970";
         String tempTime = "12:00A";
     
+        //Loop through elements, selecting each one by their names.
         while( attributeIterator.hasNext() )
         {
             Element attribute = (Element) attributeIterator.next();
@@ -160,24 +188,32 @@ public class ParseXML
             }           
         }
     
+        //Need to hand the datetime specially to wedge into a LocalDateTime 
         toReturn.dateString = tempDate + " " + tempTime;
         toReturn.date = generateDate( tempDate, tempTime );
 
         return toReturn;
     }
     
+    /*
+    * Function: generateDate
+    * Author: Marcus Haberling
+    * Uses 2 specified strings, one containing date, the other time
+    * to generate a LocalDateTime object. 
+    */
     public static LocalDateTime generateDate( String date, String time )
     {
-        time = time.replaceAll( "\\s", "" );
+        time = time.replaceAll( "\\s", "" );//Remove spaces
         date = date.replaceAll( "\\s", "" );
-        String[] dateTokens = date.split( "/" );
+        String[] dateTokens = date.split( "/" );//split on delimeters
         String[] timeTokens = time.split( ":" );
-        int year = Integer.parseInt( dateTokens[2] ) + 2000;
+        int year = Integer.parseInt( dateTokens[2] ) + 2000; //dont use before 2000
         int month = Integer.parseInt( dateTokens[0] );
         int day = Integer.parseInt( dateTokens[1] );
         int hour = Integer.parseInt( timeTokens[0] ) % 12;
-                
+        
         int minute = Integer.parseInt( timeTokens[1].substring(0,2) );
+        //If the time is PM we need to add 12 hours
         if( timeTokens[1].contains( "P" ) )
         {
             hour = hour + 12;
